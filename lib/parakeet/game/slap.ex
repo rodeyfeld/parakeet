467a924adfad1520e2d45779531b2520dc65b8c7
rodeyfeld <eric.d.rodefeld@gmail.com>
2026-02-16 01:22:57 -0500

    starting engine port

--- lib/parakeet/game/slap.ex
+++ lib/parakeet/game/slap.ex
@@ -1,17 +1,39 @@
 defmodule Parakeet.Game.Slap do
   alias Parakeet.Game.{CardStack, Card}
 
-  @type slap_types :: :no_slap | :three_in_order | :king_queen | :add_to_ten | :doubles | :sandwich
+  @type slap_type :: :no_slap | :three_in_order | :queen_king | :add_to_ten | :doubles | :sandwich
+  @spec valid_slap(CardStack.t()) :: slap_type()
+  def valid_slap(%CardStack{cards: cards}) do
+    cond do
+      doubles?(cards) -> :doubles
+      queen_king?(cards) -> :queen_king
+      sandwich?(cards) -> :sandwich
+      three_in_order?(cards) -> :three_in_order
+      adds_to_ten?(cards) -> :add_to_ten
+      true -> :no_slap
+    end
+  end
 
-  # def is_valid_slap(cards: cards), do
+  defp doubles?([top, second | _]), do: top.value == second.value
+  defp doubles?(_), do: false
 
-  # end
+  defp queen_king?([top, second | _]), do: top.face == :queen and second.face == :king
+  defp queen_king?(_), do: false
 
-  def three_in_order?(%CardStack{cards: [top, second, third | _]}) do
-    ascending = (Card.get_value_up(third) == second.value and Card.get_value_up(second) == top.value)
-    descending = (Card.get_value_down(third) == second.value and Card.get_value_down(second) == top.value)
+  defp sandwich?([top, _, third | _]), do: top.face == third.face
+  defp sandwich?(_), do: false
 
+  defp three_in_order?([top, second, third | _]) do
+    ascending = Card.get_value_up(third) == second.value and Card.get_value_up(second) == top.value
+    descending = Card.get_value_down(third) == second.value and Card.get_value_down(second) == top.value
     ascending or descending
   end
-  def three_in_order?(%CardStack{}), do: false
+  defp three_in_order?(_), do: false
+
+  defp adds_to_ten?([top, second | _]), do: top.value + second.value == 10
+  defp adds_to_ten?(_), do: false
+
+
+
+
 end
